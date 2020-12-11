77cad11fda52465fd076dc871bfd7fdcebf3d3d3
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-12-11 23:07:39 +0300

    Update Composition.cs

--- WritingPlatform/WritingPlatform.Data.Entities/Composition.cs
+++ WritingPlatform/WritingPlatform.Data.Entities/Composition.cs
@@ -1,26 +1,18 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using WritingPlatform.Base.Abstractions;
 
 namespace WritingPlatform.Data.Entities
 {
     public class Composition : BaseEntity
     {
-        [Required]
         public string Name { get; set; }
 
-        [Required]
         public string Genre { get; set; }
 
-        [Column("Publication time")]
         public DateTime PublicationTime { get; set; }
 
-        [Required]
-        [Column("User id")]
         public int UserId { get; set; }
 
-        [Required]
         public string Content { get; set; }
     }
 }
