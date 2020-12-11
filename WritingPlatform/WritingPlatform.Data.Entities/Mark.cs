1ca5ae609f22ec09012d6bafaac8954bd5970bbb
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-12-11 23:07:42 +0300

    Update Mark.cs

--- WritingPlatform/WritingPlatform.Data.Entities/Mark.cs
+++ WritingPlatform/WritingPlatform.Data.Entities/Mark.cs
@@ -1,18 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using WritingPlatform.Base.Abstractions;
+﻿using WritingPlatform.Base.Abstractions;
 
 namespace WritingPlatform.Data.Entities
 {
     public class Mark : BaseEntity
     {
-        [Required]
-        [Column("Composition id")]
         public int CompositionId { get; set; }
 
-        [Required]
-        [Column("Mark")]
-        [Range(0.0, 100.0)]
         public int Rating { get; set; }
     }
 }
