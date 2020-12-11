9dea28708f1a40b1a70394c6607861ff7947ed96
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-12-11 23:08:06 +0300

    Added MyComposition action

--- WritingPlatform/WritingPlatform/Controllers/UserController.cs
+++ WritingPlatform/WritingPlatform/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using System.Web.Security;
+using WritingPlatform.Models;
+using WritingPlatform.Models.Compositions;
 using WritingPlatform.Models.Users;
 using WritingPlatform.Service.Absractions;
 
@@ -9,10 +11,13 @@ namespace WritingPlatform.Controllers
     public class UserController : Controller
     {
         private readonly IUserService userService;
+        private readonly ICompositionService compositionService;
 
-        public UserController(IUserService userService)
+        public UserController(IUserService userService, 
+            ICompositionService compositionService)
         {
             this.userService = userService;
+            this.compositionService = compositionService;
         }
 
         [Authorize]
@@ -39,6 +44,47 @@ namespace WritingPlatform.Controllers
         public ActionResult Logout()
         {
             FormsAuthentication.SignOut();
+            return Redirect("/Account/Index");
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult MyCompositions(int page = 1)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var userModel = userService
+                    .GetUsers()
+                    .FirstOrDefault(user => user.Login == User.Identity.Name);
+
+                if (userModel != null)
+                {
+                    var compositions = compositionService
+                        .GetCompositions()
+                        .Where(comp => comp.UserId == userModel.Id);
+
+                    const int PAGE_SIZE = 30;
+
+                    var compositionsPerPages = compositions
+                        .Skip((page - 1) * PAGE_SIZE)
+                        .Take(PAGE_SIZE);
+
+                    var pageInfo = new PageInfo
+                    {
+                        PageNumber = page,
+                        PageSize = PAGE_SIZE,
+                        TotalItems = compositions.Count()
+                    };
+
+                    var ivm = new IndexViewModel<CompositionModel> 
+                    { 
+                        PageInfo = pageInfo, 
+                        Collection = compositionsPerPages 
+                    };
+
+                    return View(ivm);
+                }
+            }
             return Redirect("/Account/Index");
         }
     }
