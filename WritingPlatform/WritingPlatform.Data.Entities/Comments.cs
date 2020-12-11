859a129c647f3fe600ce0f5d901c285e7d85e71a
ILya Hadleuski <48137948+IliaGodlevsky@users.noreply.github.com>
2020-12-11 23:07:37 +0300

    Update Comments.cs

--- WritingPlatform/WritingPlatform.Data.Entities/Comments.cs
+++ WritingPlatform/WritingPlatform.Data.Entities/Comments.cs
@@ -1,16 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using WritingPlatform.Base.Abstractions;
+﻿using WritingPlatform.Base.Abstractions;
 
 namespace WritingPlatform.Data.Entities
 {
     public class Comment : BaseEntity
     {
-        [Required]
         public string Content { get; set; }
 
-        [Required]
-        [Column("Composition id")]
         public int CompositionId { get; set; }
     }
 }
